8f81b1dec72b899f3dac4d649547c8989ba6c80f
aluochvivian <aluochviviann@gmail.com>
2022-05-30 16:26:48 +0300

    first commit

---
+++ grading.py
@@ -0,0 +1,22 @@
+num = int(input("Your score:"))
+
+
+if num < 25:
+    print("F")
+elif num >=25 and num <=45:
+    print("E")
+elif num >= 45 and num <=50:
+    print("D")
+elif num >=50 and num <=60:
+    print("C")
+elif num >=60 and num <=80:
+    print("B")
+else:
+    num >=80
+    print("A")
+
+
+
+    
+
+
